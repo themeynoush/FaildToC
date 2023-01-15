353b7ad141627e662615d0fdb0c73fe0f5a37cc7
Max Well <maxwell@Mair.local>
2023-01-15 18:22:20 +0100

    read line by line and filter useful lines

--- Jan 2023/functions.h
+++ Jan 2023/functions.h
@@ -7,17 +7,6 @@
 #define _GNU_SOURCE 
 
 FILE * fp;
-//definese a pointer called line
-// char * line = NULL;
-//represents the size of an allocated block of memory. 
-//similar to a sizieof operator and strlen function
-// size_t len = 0;  
-//ssize_t = “signed size_t”.
-// ssize_t read;
-
-//strstr variable
-  //  const char *identifier = "Searcher-category--summary"; //The needle
-
 
 void downloadData();
 void openFile();
--- Jan 2023/main.c
+++ Jan 2023/main.c
@@ -1,5 +1,15 @@
 #import "functions.h"
 
+// definese a pointer called line
+char * line = NULL;
+// represents the size of an allocated block of memory. 
+// similar to a sizieof operator and strlen function
+size_t len = 0;  
+// ssize_t = “signed size_t”.
+ssize_t read;
+// strstr variable
+const char *identifier = "Searcher-category--summary"; //The needle
+
 int main() {
     //downlaods
     downloadData();
@@ -7,7 +17,12 @@ int main() {
     openFile();
     printf("done\n");
     //check datalike using strstr fuction
-    //while ((read = getline(&line, &len, fp)) != -1) 
+    while ((read = getline(&line, &len, fp)) != -1) {
+        //if you found the identifier in line, print the following:
+        if (strstr(line, identifier)) {
+            printf("hi");
+        }
+    }
     //if check data line ()
     //rec=get fileds as input
     //records [i] = rec
